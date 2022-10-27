194512028aa609e43421a86e131b2c13e9440cc6
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-27 21:14:07 +0200

    Add DispatchQueues

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -30,17 +30,24 @@ class ViewController: UITableViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(askFilter))
         
-        DispatchQueue.global(qos: .userInitiated).async {
-            [weak self] in // "[weak self] in" it isn't necessary here because Grand Central Dispatch (GCD) runs the code once then throws it away but I'm writing it anyway
+        // push code to a background thread - it's OK to parse the JSON on a background thread
+        // by downloading data from the internet in viewDidLoad() will lock up until all the data has been transferred so it's gonna block the UI therefore i am using here one of the GCD functions, which allows me to fetch the data without locking up the UI
+        DispatchQueue.global(qos: .userInitiated).async { // userInitiated: this will execute tasks requested by the user that they are now waiting for in order to continue using the app
+            [weak self] in // "[weak self] in" it isn't necessary here because Grand Central Dispatch (GCD) runs the code once then throws it away but I wrote it anyway
             if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url) {
+                if let data = try? Data(contentsOf: url) { // Data's contentsOf to download data from the internet, which is what's known as a blocking call. It blocks execution of any further code in the method until it has connected to the server and fully downloaded all the data, that's why i am using above DispatchQueue.global(qos: .userInitiated).async
                     self?.parse(json: data)
-                    return
+                    return // because it's returning from the closure that was being executed asynchronously, it effectively does nothing, therefore regardless of whether the download succeeds or fails, the showError() down below will be called. And if the download succeeds, the JSON will be parsed on the background thread and the table view's reloadData() will be called on the background thread (which is absolutely NOT OK) and the error will be shown regardless
                 }
             }
+            // it will no longer blocks the main thread while the JSON downloads from the internet but therefore now I am pushing work to the background thread and any further code called in that work ( which is func parse(json: Data) and func showError() ) will also be on the background thread so I fixed those too
+            
+            // to stop showError() being called regardless of the result of fetch call I moved the showError here in DispatchQueue.global() in viewDidLoad()
+                self?.showError() // and because it is inside a closure now, I added "self." to make capturing clear
+            // Now the showError runs in the background thread but inside showError() method there is UIAlertController that means now UI work happening on a background thread which is NOT OK so I fixed the method too down there
+            
+            // I moved the showError right above
         }
-        
-        showError()
     }
     
     @objc func askFilter() {
@@ -84,9 +91,13 @@ class ViewController: UITableViewController {
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        // because I moved showError() inside DispatchQueue.global() in viewDidLoad() the UI work happening on a background so I had to modify showError() to push that work back to the main thread
+        DispatchQueue.main.async { [weak self] in
+            let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(ac, animated: true) // now it is inside a closure, I added "self." to make capturing clear
+        }
+        
     }
     
     func parse(json: Data) {
@@ -94,7 +105,13 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.result
-            tableView.reloadData()
+            
+            // push code back to the main thread - update the UI safely on the main thread
+            // Here the table view reloaded and because of it's never OK to do UI work on the background thread i place the async() here so the code execute on the main thread
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+            
         }
     }
     
